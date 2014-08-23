b5f42fd8bdbb1d335e90ebef25a583e3a21a8a84
sridharxp <aurosridhar@gmail.com>
2014-08-23 21:29:30 +0530

    json2xml converter ready, though Url Decoding and Unicode issues may be present.

---
+++ json/json2xml.dpr
@@ -0,0 +1,47 @@
+program json2xml;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System,
+  SysUtils,
+  Json,
+  lexlib,
+  Dialogs,
+  bjxml2 in '\..\DL\bjxml\bjxml2.pas';
+
+var
+  xdb: IbjXml;
+  jsonFN: string;
+  xmlFN: string;
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+  if ParamCount = 0 then
+  begin
+    writeln('Sytax: json2xml jsonFileName xmlFileName');
+    Exit;
+  end;
+  if ParamCount = 1 then
+  begin
+    writeln('Sytax: json2xml jsonFileName xmlFileName');
+    jsonFN := ParamStr(1);
+    xmlFN := 'xml.txt';
+  end;
+  if ParamCount = 2 then
+  begin
+    jsonFN := ParamStr(1);
+    xmlFN := ParamStr(2);
+  end;
+  AssignFile(yyinput, jsonFN);
+  FileMode := 0;  {Set file access to read only }
+  Reset(yyinput);
+  AssignFile(yyoutput, xmlFN);
+  FileMode := fmOpenRead or fmShareDenyWrite;
+  Rewrite(yyoutput);
+//  while yyparse = 0 do
+  yyparse;
+//  xdb := TbjXml.Create;
+//  xdb.LoadXml(xmlText);
+//  ShowMessage(xdb.GetXml);
+//  xdb.SaveXmlFile('xml.txt');
+end.
